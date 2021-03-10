4ac7f5d07380e6e710782955d98def476cf6957c
MiguelUrena <78118827+MiguelUrena@users.noreply.github.com>
2021-03-09 23:37:40 -0500

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -13,11 +13,12 @@ def hangman(tries,word,guesses):
         failed=0
         for character in word:
             if character in guesses:
-                print(character)
+                print(character,end=" ")
             else:
-                print("_")
+                print("_",end=" ") 
                 failed=failed+1
         if failed==0:
+            print("\n")
             print("Congrats, You Won!")
             print(f"The Word was: {word} ")
             repeat=input("Would you like to Play Again?(y/n): ")
@@ -28,44 +29,47 @@ def hangman(tries,word,guesses):
                 exit()
             else:
                 print("Invalid Input")
-                exit()    
+                exit() 
+        print("\n")   
         guess=input("Guess a Character of the Word: ")
         if guess.isalpha()==False:
-            print("enter only letters")
+            print("Enter Only Letters")
+            print(f"You have {tries} tries remaining.")
         elif len(guess)>1:
-            print("enter only a single letter")
+            print("Enter Only a Single Letter")
+            print(f"You have {tries} tries remaining.")
         else:
             guesses=guesses+guess
             guesses=guesses.lower()
-        if guess in word:
-            if guess in characters:
-                characters.remove(guess)
-                print(characters)
+            if guess in word:
                 print("Correct, Keep Going!")
-            elif guess not in characters:
-                tries=tries+1
-        if guess not in word:
-            tries=tries-1
-            
-            print(f"You have {tries} tries remaining.")
-            if guess in characters:
-                characters.remove(guess)
-                print(characters)
-                print("Wrong Guess")
-            elif guess not in characters:
-                print("You Already Guessed That, Try Again")
-                tries=tries+1
-        if tries==0:
-            print("You Lost :(")
-            repeat=input("Would you like to Play Again?(y/n): ")
-            if repeat=="y":
-                hangman(8,get_word(possible_words),'')
-            elif repeat=="n":
-                print("Goodbye!")
-                exit()
-            else:
-                print("Invalid Input")
-                exit()    
+                if guess not in characters:
+                    print("You Have Already Guessed This Letter")
+                    print(f"You have {tries} tries remaining.")
+                else:
+                    characters.remove(guess)
+                    print(characters)
+            if guess not in word:
+                    print("Wrong Guess")
+                    if guess not in characters:
+                        print("You Have Already Guessed This Letter")
+                        print(f"You have {tries} tries remaining.")
+                    else:
+                        tries=tries-1
+                        characters.remove(guess)
+                        print(characters)
+                        print(f"You have {tries} tries remaining.")
+            if tries==0:
+                print("You Lost :(")
+                repeat=input("Would you like to Play Again?(y/n): ")
+                if repeat=="y":
+                    hangman(8,get_word(possible_words),'')
+                elif repeat=="n":
+                    print("Goodbye!")
+                    exit()
+                else:
+                    print("Invalid Input")
+                    exit()    
     
         
 
